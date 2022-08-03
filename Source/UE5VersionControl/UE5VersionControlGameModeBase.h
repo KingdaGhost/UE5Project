3b067fe71be2e364995af070c7c3966e86a78a5a
Bryan <bryankhongdeit@gmail.com>
2022-08-03 10:19:09 +0530

    Creating Unreal Engine 5 VC

---
+++ Source/UE5VersionControl/UE5VersionControlGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5VersionControlGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5VERSIONCONTROL_API AUE5VersionControlGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
